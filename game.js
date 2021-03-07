0fe49a7a71aa485ab11ca358f8b2b6b72a95799c
RASalamanca <robertoandres24@yahoo.com>
2021-03-07 10:26:07 -0800

    1 file modified

--- game.js
+++ game.js
@@ -1,4 +1,147 @@
-//config objet
+//Boot Scene
+//This is the scene we use to delcare global information that we reuse trought the Game
+//Mostly animation data
+var BootScreen = new Phaser.Class({
+  Extends: Phaser.Scene,
+
+  initialize: function BootScreen(){
+    Phaser.Scene.call(this, {key: 'bootScreen'});
+  },
+
+  init: function () {},
+
+  preload: function () {
+
+    //loads up spritesheets
+    this.load.spritesheet('player', 
+    'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/sprites/Zack.png',
+    { frameWidth: 24, frameHeight: 32});
+
+    this.load.spritesheet('bandit', 
+    'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/sprites/Bandido.png',
+    { frameWidth: 24, frameHeight: 32});
+  },
+
+  create: function () {
+
+    //Player sprite animations
+    {
+     //Walking animation
+     {
+        this.anims.create({               // <--Walk Up
+          key: 'walkUp',
+          frames: this.anims.generateFrameNumbers('player', {start: 10, end: 12}),
+          frameRate: 10,
+          repeat: -1
+        });
+        this.anims.create({               // <--Walk Right
+          key: 'walkRight',
+          frames: this.anims.generateFrameNumbers('player', {start: 22, end: 24}),
+          frameRate: 10,
+          repeat: -1
+        });
+        this.anims.create({               // <--Walk Down
+          key: 'walkDown',
+          frames: this.anims.generateFrameNumbers('player', {start: 34, end: 36}),
+          frameRate: 10,
+          repeat: -1
+        });
+        this.anims.create({               // <--Walk Left
+          key: 'walkLeft',
+          frames: this.anims.generateFrameNumbers('player', {start: 46, end: 48}),
+          frameRate: 10,
+          repeat: -1
+        });
+      }
+
+      //Standing animations
+      {
+        this.anims.create({               // <--Stand Up
+          key: 'StandUp',
+          frames: [{key: 'player', frame: 11}],
+          frameRate: 20,
+        });
+        this.anims.create({               // <--Stand Right
+          key: 'StandRight',
+          frames: [{key: 'player', frame: 23}],
+          frameRate: 20,
+        });
+        this.anims.create({               // <--Stand Down
+          key: 'StandDown',
+          frames: [{key: 'player', frame: 35}],
+          frameRate: 20,
+        });
+        this.anims.create({               // <--Stand Left
+          key: 'StandLeft',
+          frames: [{key: 'player', frame: 47}],
+          frameRate: 20,
+        });
+      }
+    }
+  },
+
+  update: function () {},
+});
+
+//Main Scene
+var FloatingIsland = new Phaser.Class({
+  Extends: Phaser.Scene,
+
+  initialize: function FloatingIsland(){
+    Phaser.Scene.call(this, {key: 'floatingIsland'});
+  },
+
+  init: function () {},
+
+  preload: function () {
+
+  	//loads floating island tileset & Tilemap
+  	this.load.image('Island', 'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/tilesets/floatingIslands.png');
+  
+  	this.load.tilemapTiledJSON('map', 'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/maps/Area2.json');
+  },
+
+  create: function () {
+
+  	//renders level map
+  	const map = this.make.tilemap({ key: "map" });
+  	const tileset = map.addTilesetImage("Island", "Island");
+	
+	  //draws map layers
+	  this.clifLayer = map.createLayer("Cliffs", tileset, 0, 0);
+	  this.baseLayer = map.createLayer("Base", tileset, 0, 0);
+	  this.detailLayer = map.createLayer("Details", tileset, 0, 0);
+
+    //map colliders
+    this.baseLayer.setCollisionByProperty({ Collides: true });
+    this.detailLayer.setCollisionByProperty({ Collides: true });
+   
+  	//Creates Camera and camera controls
+  	const camera = this.cameras.main;
+		
+	  const cursors = this.input.keyboard.createCursorKeys();
+	  controls = new Phaser.Cameras.Controls.FixedKeyControl({
+  		camera: camera,
+	    left: cursors.left,
+		  right: cursors.right,
+		  up: cursors.up,
+		  down: cursors.down,
+		  speed: 0.5
+		});
+
+	  //costrains camera to the inside of tilemap
+	  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+  }, 
+
+  update: function (time, delta) {
+
+	  //apply the controls to the camera each tick of the game
+	  controls.update(delta);
+	
+  }
+});
+
+//Game config objet
 var config = {
 	type: Phaser.AUTO,
 	width: 420,
@@ -10,70 +153,8 @@ var config = {
     default: "arcade",
     gravity: { y: 0}
   },
-  scene: {
-		preload: preload,
-		create: create,
-		update: update
-	}
+  scene: [FloatingIsland]
 };
-
-//Scene Globals
-var controls;
 
 //creates the game and injects the canvas element
 var game = new Phaser.Game(config);
-
-function preload () {
-
-	//loads floating island tileset & Tilemap
-	this.load.image('Island', 'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/tilesets/floatingIslands.png');
-  
-	this.load.tilemapTiledJSON('map', 'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/maps/Area2.json');
-
-  //loads up spritesheets
-  this.load.spritesheet('player', 
-  'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/sprites/Zack.png',
-  { frameWidth: 24, frameHeight: 32});
-
-  this.load.spritesheet('bandit', 
-  'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/sprites/Bandido.png',
-  { frameWidth: 24, frameHeight: 32});
-}
-
-function create () {
-	//renders level map
-	const map = this.make.tilemap({ key: "map" });
-	const tileset = map.addTilesetImage("Island", "Island");
-	
-	//draws map layers
-	this.clifLayer = map.createLayer("Cliffs", tileset, 0, 0);
-	this.baseLayer = map.createLayer("Base", tileset, 0, 0);
-	this.detailLayer = map.createLayer("Details", tileset, 0, 0);
-
-  //map colliders
-  this.baseLayer.setCollisionByProperty({ Collides: true });
-  this.detailLayer.setCollisionByProperty({ Collides: true });
-   
-	//Creates Camera and camera controls
-	const camera = this.cameras.main;
-		
-	const cursors = this.input.keyboard.createCursorKeys();
-	controls = new Phaser.Cameras.Controls.FixedKeyControl({
-		camera: camera,
-		left: cursors.left,
-		right: cursors.right,
-		up: cursors.up,
-		down: cursors.down,
-		speed: 0.5
-		});
-
-	//costrains camera to the inside of tilemap
-	camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-}
-
-function update (time, delta) {
-
-	//apply the controls to the camera each tick of the game
-	controls.update(delta);
-	
-}
