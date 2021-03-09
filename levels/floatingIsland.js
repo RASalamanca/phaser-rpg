7464dd6e090c3d179ba338b0c070797c0577fea8
RASalamanca <robertoandres24@yahoo.com>
2021-03-09 06:09:56 -0800

    1 file modified

--- levels/floatingIsland.js
+++ levels/floatingIsland.js
@@ -8,14 +8,14 @@ var FloatingIsland = new Phaser.Class({
 
   //Scene Globals
   player: undefined,
-  playerHead: undefined,
-  overlapBody: false,
-  overlapHead: false,
   cursors: undefined,
+  fogLayer: undefined,
 
   init: function () {},
 
   preload: function () {
+    //Loads fog texture (current texture is only a placeholder)
+    this.load.image('fog', 'https://genekogan.com/code/p5js-perlin-noise/perlin2.png');
         
   	//loads floating island tileset & Tilemap
   	this.load.image('Island', 'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/tilesets/floatingIslands.png');
@@ -32,11 +32,14 @@ var FloatingIsland = new Phaser.Class({
 	  //draws map layers
 	  this.cliffUnderLayer = map.createLayer("CliffsUnder", tileset, 0, 0);
     this.cliffBelowLayer = map.createLayer("CliffsBelow", tileset, 0, 0);
-	  this.baseLayer = map.createLayer("Base", tileset, 0, 0);
+    fogLayer = this.add.tileSprite(300, 360, 840, 720, 'fog');
+    fogLayer.setAlpha(0.34, 0.24, 0.41, 0.31);
+    //fogLayer.setAlpha(0.34, 0.34, 0.34, 0.34);
+    fogLayer.setScrollFactor(0, 1);
+    this.baseLayer = map.createLayer("Base", tileset, 0, 0);
     this.baseUpperLayer = map.createLayer("BaseUpper", tileset, 0, 0);
 	  this.detailLayer = map.createLayer("Details", tileset, 0, 0);
-    this.baseUpperLayer.setDepth(20)
-    this.detailLayer.setDepth(23);
+    this.detailLayer.setDepth(20);
 
     //Creates Player
     //The drawing order should depend on the Y cordinate, but that is to complicated.
@@ -46,63 +49,33 @@ var FloatingIsland = new Phaser.Class({
     player = this.physics.add.sprite( spawnPoint.x, spawnPoint.y, 'player', 34);
     player.setBodySize(8, 8, true);
     player.setOffset(8, 24);
-    player.setDepth(21);
-    playerHead = this.physics.add.image( spawnPoint.x, spawnPoint.y);
-    playerHead.setBodySize(14, 16, true);
-    playerHead.setOffset(0, -8);
-    playerHead.setDepth(22);
 
-    //Player/map overlap
-    overlapHead = false;
-    overlapBody = false;
-
-    player.on('overlapstart', function() {
-      overlapBody = true;
-      console.log("body is overlaping");
-    });
-    player.on('overlapend', function() {
-      overlapBody = false;
-      onsole.log("body is not overlaping");
-    });
-    playerHead.on('overlapstart', function() {
-      overlapHead = true;
-      console.log("head is overlaping");
-    });
-    player.on('overlapend', function() {
-      overlapHead = false;
-      console.log("head is not overlaping");
-    });
 
     //map colliders
+    this.cliffBelowLayer.setCollisionByProperty({ Collides: true });
     this.baseLayer.setCollisionByProperty({ Collides: true });
     this.baseUpperLayer.setCollisionByProperty({ Collides: true });
     this.detailLayer.setCollisionByProperty({ Collides: true });
+    this.physics.add.collider( player, this.cliffBelowLayer);
     this.physics.add.collider( player, this.baseLayer);
     this.physics.add.collider( player, this.baseUpperLayer);
     this.physics.add.collider( player, this.detailLayer);
-    this.physics.add.overlap( player, this.detailLayer, console.log("body is overlaping"), null, this);
-    this.physics.add.overlap( playerHead, this.detailLayer);
 
     //Assigns cursors
 	  cursors = this.input.keyboard.createCursorKeys();
 
     //Creates Camera, asigns it follow player and costrains it to the inside of tilemap
   	const camera = this.cameras.main;
+    camera.setBackgroundColor('#03021c');
     camera.startFollow(player);
 	  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
   }, 
 
   update: function (time, delta) {
-    if (overlapHead && !overlapBody) {
-      player.setDepth(24);
-      playerHead.setDepth(25);
-    }
-    if (!overlapHead && !overlapBody) {
-      player.setDepth(21);
-      playerHead.setDepth(22);
-    }
-    
 
+    //fogLayer.setTilePosition();
+    fogLayer.tilePositionX += 5;
+    fogLayer.tilePositionY -= 2;
 
     //Player controls---------------------------------------------------
     const speed = 112;
@@ -110,30 +83,24 @@ var FloatingIsland = new Phaser.Class({
 
     // Stop any previous movement from the last frame
     player.body.setVelocity(0);
-    playerHead.body.setVelocity(0);
 
     // Horizontal movement
     if (cursors.left.isDown) {
       player.body.setVelocityX(-speed);
-      playerHead.body.setVelocityX(-speed);
     } else if (cursors.right.isDown) {
       player.body.setVelocityX(speed);
-      playerHead.body.setVelocityX(speed);
     }
 
     // Vertical movement
     if (cursors.up.isDown) {
       player.body.setVelocityY(-speed);
-      playerHead.body.setVelocityY(-speed);
     } else if (cursors.down.isDown) {
       player.body.setVelocityY(speed);
-      playerHead.body.setVelocityY(speed);
     }
 
     // Normalize and scale the velocity so that player can't move faster along a diagonal
     player.body.velocity.normalize().scale(speed);
-    playerHead.body.velocity.normalize().scale(speed);
-
+    
     // Update the animation last and give left/right animations precedence over up/down animations
     if (cursors.left.isDown) {
       player.anims.play("walkLeft", true);
