f34eee176e762c29e276e6404034151db90fa1a0
RASalamanca <robertoandres24@yahoo.com>
2021-03-09 13:01:09 -0800

    1 file modified

--- RPGcombat.js
+++ RPGcombat.js
@@ -13,24 +13,43 @@ function Armor(name, level, healthPoints, defence, thoughness){
   this.thoughness = thoughness;
 }
 
-function Skill(name, elements, cost){
+function Skill(name, cost, elements, affinities){
   this.name = name;
   this.elements = elements;
+  this.eleAffinity = eleAffinity
   this.cost = cost;
   this.damageMultiplier = ( 0.5 * (cost ^ 2) ) + ( 0.5 * cost);
 
-  //assigns elemental damage multipliers based on the skill's elements
+  //assigns elemental multipliers based on the skill's elements
   var elementalMult = 1 / this.elements.lenght;
   this.fire = this.elements.includes('fire') ? elementalMult : 0;
   this.ice = this.elements.includes('ice') ? elementalMult : 0;
   this.lightning = this.elements.includes('lightning') ? elementalMult : 0;
   this.chaos = this.elements.includes('chaos') ? elementalMult : 0;
   this.physical = this.elements.includes('physical') ? elementalMult : 0;
+
+  //assigns elemental affinities based on the skill's affinities
+  var affinitylMult = 1 / this.affinities.lenght;
+  this.fireAff = this.affinities.includes('fire') ? affinityMult : 0;
+  this.iceAff = this.affinities.includes('ice') ? affinityMult : 0;
+  this.lightningAff = this.affinities.includes('lightning') ? affinityMult : 0;
+  this.chaosAff = this.affinities.includes('chaos') ? affinityMult : 0;
+  this.physicalAff = this.affinities.includes('physical') ? affinityMult : 0;
   
   //only basic damage skills so far.
   this.use = function(user, target){
+
+    //energy cost of the skill
     user.energy -= this.cost;
 
+    //elemental multiplier gains and losses from using the skill
+    user.fireMult += this.cost * ( this.fireAff - this.fire);
+    user.iceMult += this.cost * ( this.iceAff - this.ice);
+    user.lightningMult += this.cost * ( this.lightningAff - this.lightning);
+    user.chaosMult += this.cost * ( this.chaosAff - this.chaos);
+    user.physicalMult += this.cost * ( this.physicalAff - this.physical);
+
+    //Damage calculations
     var damageRange = ( ser.maxDamade - user.minDamage ) + 1;   //Math.random always returns a number less than 1
     var baseDamage = user.minDamage + Math.floor(Math.random() * damageRange);
     var skillDamage = baseDamage * this.damageMultiplier;        
