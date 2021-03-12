46275576723f0a91d73b0663cac007676f0b4d62
RASalamanca <robertoandres24@yahoo.com>
2021-03-12 08:36:36 -0800

    2 files modified, 1 added

---
+++ levels/FightScreen.js
@@ -0,0 +1,23 @@
+//This is the fight screen UI. 
+//It is to be called on combat start.
+
+var FightScreen = new Phaser.Class({
+  Extends: Phaser.Scene,
+
+  initialize: function BootScreen(){
+    Phaser.Scene.call(this, {key: 'FightScreen'});
+  },
+
+  preload: function () {
+    //load up Skilll icons and other UI elements
+  },
+
+  create: function () {
+    //Draw UI elements on screen.
+  },
+
+  update: function () {
+
+  },
+
+});
