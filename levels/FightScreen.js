f5b4a2da4d3f5d4286c21f96330d990986b416f7
RASalamanca <robertoandres24@yahoo.com>
2021-03-17 06:35:40 -0700

    1 file modified

--- levels/FightScreen.js
+++ levels/FightScreen.js
@@ -10,6 +10,32 @@ var FightScreen = new Phaser.Class({
 
   preload: function () {
     //load up Skilll icons and other UI elements
+
+    //Armor and HP bar elements
+    this.load.image( 'ArmorBarFrame', 
+    'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/UIElements/PlayerArmorBarBox.png');
+    this.load.image( 'ArmorBarBack', 
+    'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/UIElements/PlayerArmorBarBack.png');
+    this.load.image( 'ArmorBar', 
+    'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/UIElements/PlayerArmorBar.png');
+    this.load.image( 'Heart', 
+    'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/UIElements/Hearth.png');
+    this.load.image( 'Star', 
+    'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/UIElements/Star.png');
+    this.load.image( 'DivisionSign', 
+    'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/UIElements/DivisionSign.png');
+    this.load.image( 'PercentageSign', 
+    'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/UIElements/PercentageSign.png');
+
+    this.load.image( 'PortraitPlaceholder', 
+    'https://raw.githubusercontent.com/RASalamanca/phaser-rpg/master/assets/UIElements/PlayerPortraitPlaceHolder.png');
+
+
+
+
+
+
+
   },
 
   create: function () {
